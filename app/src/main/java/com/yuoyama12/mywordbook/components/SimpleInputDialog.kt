05a749ef64034d38b862ebc6b10b0b92b99e6184
YuOyama12 <yuoyama.5@gmail.com>
2022-09-03 11:58:11 +0900

    [Added]単語帳を追加するためのダイアログレイアウト

---
+++ app/src/main/java/com/yuoyama12/mywordbook/components/SimpleInputDialog.kt
@@ -0,0 +1,91 @@
+package com.yuoyama12.mywordbook.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.yuoyama12.mywordbook.R
+
+private val Spacer = Modifier.padding(vertical = 3.dp)
+private val MessagePadding = Modifier.padding(bottom = 28.dp)
+
+@Composable
+fun SimpleInputDialog(
+    title: String,
+    message: String = "",
+    preInputtedText:String = "",
+    textFieldHint: String,
+    positiveButtonText: String,
+    onDismissRequest: () -> Unit,
+    onPositiveButtonClicked: (String) -> Unit
+) {
+    var inputtedText by rememberSaveable { mutableStateOf(preInputtedText) }
+
+    Dialog(
+        onDismissRequest = { onDismissRequest() }
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colors.surface
+        ) {
+            Box (
+                modifier = Modifier.padding(horizontal = 24.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column {
+                    Spacer(Modifier.padding(vertical = 8.dp))
+
+                    Text(
+                        text = title,
+                        modifier = Modifier.alpha(ContentAlpha.high),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+
+                    Spacer(Spacer)
+
+                    if (message != "") {
+                        Text(
+                            text = message,
+                            modifier = MessagePadding.alpha(ContentAlpha.medium),
+                            style = MaterialTheme.typography.body2
+                        )
+
+                        Spacer(Spacer)
+                    }
+
+                    OutlinedTextField(
+                        value = inputtedText,
+                        onValueChange = { inputtedText = it },
+                        placeholder = { Text(textFieldHint) },
+                        singleLine = true
+                    )
+
+                    Spacer(Spacer)
+
+                    Row(
+                        modifier = Modifier.align(Alignment.End),
+                        horizontalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        TextButton(onClick = { onDismissRequest() }) {
+                            Text(stringResource(R.string.dialog_cancel))
+                        }
+                        TextButton(
+                            onClick = {
+                                onPositiveButtonClicked(inputtedText)
+                                onDismissRequest()
+                            }
+                        ) {
+                            Text(positiveButtonText)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/yuoyama12/mywordbook/ui/wordbooks/WordbooksScreen.kt
+++ app/src/main/java/com/yuoyama12/mywordbook/ui/wordbooks/WordbooksScreen.kt
@@ -3,11 +3,14 @@ package com.yuoyama12.mywordbook.ui.wordbooks
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import com.yuoyama12.mywordbook.R
+import com.yuoyama12.mywordbook.components.SimpleInputDialog
 
 @Composable
 fun WordbooksScreen() {
@@ -22,6 +25,31 @@ fun WordbooksScreen() {
                     )
                 }
             )
+        },
+        floatingActionButton = {
+            var openAddDialog by remember { mutableStateOf(false) }
+
+            FloatingActionButton(
+                onClick = { openAddDialog = true },
+            ) {
+                Icon(
+                    Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.fab_add_desc),
+                )
+
+                if (openAddDialog) {
+                    SimpleInputDialog(
+                        title = stringResource(R.string.dialog_add_wordbook_title),
+                        textFieldHint = stringResource(R.string.dialog_add_wordbook_hint),
+                        positiveButtonText = stringResource(R.string.dialog_add_workbook_positive_button),
+                        onDismissRequest = { openAddDialog = false }
+                    ) { inputtedText ->
+
+
+                    }
+                }
+
+            }
         }
     ) { padding ->
         Column(
